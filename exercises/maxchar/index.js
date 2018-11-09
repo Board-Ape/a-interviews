d7153055afb4ab6be8ecfa8a355560c6d148577a
Sam Singer <samuel.singer1@gmail.com>
2018-11-09 13:15:12 -0800

    MaxChar with o(n) time and space

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Solve: Space O(n) Time O(n)
+function maxChar(str) {
+    const cache = {};
+
+    for (let char of str) {
+        if (!cache[char]) {
+            cache[char] = 1;
+        } else {
+            cache[char]++;
+        }
+    }
+
+    let maxCharValue = 0;
+    let charResult = '';
+
+    Object.keys(cache).forEach(char => {
+        if (cache[char] > maxCharValue) {
+            maxCharValue = cache[char];
+            charResult = char
+        }
+    });
+
+    return charResult;
+}
 
 module.exports = maxChar;
