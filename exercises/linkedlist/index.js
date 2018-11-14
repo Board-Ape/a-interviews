cd06592f4dcaa6388cd0b2217a1f60e9abdd953a
Sam Singer <samuel.singer1@gmail.com>
2018-11-13 17:05:40 -0800

    Linked List initial commit

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -7,3 +7,34 @@ class Node {}
 class LinkedList {}
 
 module.exports = { Node, LinkedList };
+/* 
+
+Linked List:
+
+class Node {
+    constructor (data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor () {
+        this.head = null;
+        this.length = 0;
+    }
+
+    addNodeEnd(data) {
+        
+    }
+
+    find() {
+
+    }
+
+    delete() {
+
+    }
+}
+
+*/
