fc8720ab2f2ae950b50bb44e0bdc4b7d3a9a45fd
Sam Singer <samuel.singer1@gmail.com>
2018-11-18 20:04:20 -0800

    Solid working code with if/else

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -21,7 +21,20 @@
 // Are we taking into consideration negative values as well?
 
 function fizzBuzz(n) {
-
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 === 0) {
+            console.log('fizzbuzz');
+        }
+        else if (i % 5 === 0) {
+            console.log('buzz');
+        }
+        else if (i % 3 === 0) {
+            console.log('fizz');
+        }
+        else {
+            console.log(i)
+        }
+    }
 }
 
 module.exports = fizzBuzz;
