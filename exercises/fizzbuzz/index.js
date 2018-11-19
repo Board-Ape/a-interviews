2efd29a5d0f0567d66c0db23dd08a979b0d49df3
Sam Singer <samuel.singer1@gmail.com>
2018-11-18 20:19:11 -0800

    Look into building switch statements with operations within

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -22,19 +22,37 @@
 
 function fizzBuzz(n) {
     for (let i = 1; i <= n; i++) {
-        if (i % 15 === 0) {
-            console.log('fizzbuzz');
-        }
-        else if (i % 5 === 0) {
-            console.log('buzz');
-        }
-        else if (i % 3 === 0) {
-            console.log('fizz');
-        }
-        else {
-            console.log(i)
+        switch (i) {
+            case i % 15 === 0:
+                console.log('fizzbuzz');
+                break;
+            case i % 5 === 0:
+                console.log('buzz');
+                break;
+            case i % 3 === 0:
+                console.log('fizz');
+                break;
+            default:
+                console.log(i);
         }
     }
 }
+
+// function fizzBuzz(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 15 === 0) {
+//             console.log('fizzbuzz');
+//         }
+//         else if (i % 5 === 0) {
+//             console.log('buzz');
+//         }
+//         else if (i % 3 === 0) {
+//             console.log('fizz');
+//         }
+//         else {
+//             console.log(i)
+//         }
+//     }
+// }
 
 module.exports = fizzBuzz;
