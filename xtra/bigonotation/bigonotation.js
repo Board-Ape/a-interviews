3a3ae06e66b28c2dd9b7fa3d5b0518ade83a76ba
Sam Singer <samuel.singer1@gmail.com>
2018-11-19 19:11:15 -0800

    Remember to drop the constants and relatives

--- xtra/bigonotation/bigonotation.js
+++ xtra/bigonotation/bigonotation.js
@@ -21,3 +21,18 @@ function print(items) {
         item.forEach(unit => console.log(item : unit));
     });
 }
+
+// Drop Constants 0(2n) => 0(n)
+function print(items, basics) {
+    items.forEach(item => console.log(item));
+
+    basics.forEach(basic => console.log(basic));
+}
+
+// Drop Less Signigicant Terms 0(n + n^2) => 0(n^2)
+function print(items, basics) {
+    items.forEach(item => console.log(item));
+
+    basics.forEach(itemOne => itemOne.forEach(itemTwo => console.log(itemTwo)));
+} 
+
