95f92b9de5e6ae9d3fbc5abbb29fb4d960c01448
Sam Singer <samuel.singer1@gmail.com>
2018-11-20 11:46:52 -0800

    First test passing JEST

--- exercises/phonechars/test.js
+++ exercises/phonechars/test.js
@@ -5,3 +5,10 @@ test('Letters is a class', () => {
     expect(typeof Letters.prototype.constructor).toEqual('function');
 });
 
+describe('Get Numbers', () => {
+    test('retrieve values possible letter values', () => {
+        const n = new Letters();
+        const letterPossibilities = n.getLetters('1');
+        expect(letterPossibilities).toEqual(['a','b','c']);
+    });
+});
