a27a83644c2414e4d7a4b8590d6bd12ea92421a3
Sam Singer <samuel.singer1@gmail.com>
2018-11-21 12:51:41 -0800

    Binary numbers

---
+++ xtra/datastructures/ram.js
@@ -0,0 +1,25 @@
+/*
+
+Random Access Memory
+    - As a computer runs code it tracks (numbers, strings, arrays etc.)
+    - Variables of this nature are stored here.
+
+    Processor:
+    - Does all the work inside you computer
+        Cache:
+        - Stores a copy of recently read frm RAM
+
+    Memory Controller:
+    - Does the actual reading and writing to and from RAM.
+    - Direct connection to each shelf.
+        - That's critical since we can access at 0(1).
+        - There are performance increases when memory is nearby
+            - When contents are retrieved for a particular address, a handful or nearby memory addresses are also sent
+
+    RAM Stack
+    - Analogy of a Tall Bookcase
+    - Shelves are numbered
+    - The number os the address
+    - Each shelf holds 8 bits made up of 1's and 0'sf
+   
+ */
