28c3671678b2ac077c038b948a68c76ed699a891
Sam Singer <samuel.singer1@gmail.com>
2018-11-21 13:01:46 -0800

    fixed width integers

---
+++ xtra/datastructures/fixedwidth.js
@@ -0,0 +1,13 @@
+/*
+
+Fixed Width Integers
+    - How many integers can you expres with 1 byte(8 bits) = 2^8 256 different numbers
+    - 255 = 1111 1111 so 256 would be 1 1111 1111 
+    Integer Overflow: 
+        - Javascript converts the unit to infinity if it gets too large
+    
+    - Instead of using 1 byte we usually use 4 or 8 bytes (32 or 64 bits)
+
+    - Most integers are fixed width or fixed length
+
+*/
