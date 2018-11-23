4b66bd2bc483ec5903a6eff911f1148f2a90486a
Sam Singer <samuel.singer1@gmail.com>
2018-11-22 21:26:15 -0800

    So the tradeoffs continue from the dynamic arrays to the linkedlists. Fast prepends and appends but slower lookups

---
+++ xtra/datastructures/linkedlists.js
@@ -0,0 +1,13 @@
+/*
+
+Linked Lists
+
+- Discussed before in the Dynamic Arrays. These have a time cost of slower appends. Creating a word processor we want fast appends.
+
+- Create nodes that hold the data of each letter and then has pointer that points to the next node or letter of the word.
+
+- NOT Cache friendly though
+
+- Have fast prepends and appends BUT slow look-ups.
+
+*/
