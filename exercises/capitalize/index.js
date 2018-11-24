3405d04cb8c7b83983432e313df53d721ae1675a
Sam Singer <samuel.singer1@gmail.com>
2018-11-23 23:50:03 -0800

    Handle conditions for no string

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,15 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// Can we assume that we always receive a string?
+// No: Error handling at the start will test for type.
+// Can we assume there are always characters present?
+// No: Error Handle for empty string
+
+function capitalize(str) {
+    if (typeof str !== 'string') {
+        return "Input is NOT a string";
+    }
+}
 
 module.exports = capitalize;
