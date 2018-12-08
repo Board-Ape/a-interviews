2cc95e03bf263ee8f42ecc6da60834794ee6b077
Sam Singer <samuel.singer1@gmail.com>
2018-12-08 13:12:21 -0800

    More on trees

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -34,41 +34,102 @@ class Node {
     }
 
     add(data) {
-        this.children.push(new Node(data));
+        this.children.push(new Node(data))
     }
 
     remove(data) {
-        this.children = this.children.filter(node => {
-            return node.data !== data;
-        });
+        this.children = this.children.filter(function(node) {
+            return node.data !== data
+        })
     }
 }
 
 class Tree {
-    constructor() {
-        this.root = null;
-    }
 
-    traverseBF(fn) {
-        const result = [ this.root ];
-
-        while (result.length) {
-            const node = result.shift();
-            result.push(...node.children);
-            fn(node);
-        }
-
-    }
-
-    traverseDF(fn) {
-        const result = [ this.root ];
-
-        while (result.length) {
-            const node = result.shift(); 
-            result.unshift(...node.children);
-            fn(node);
-        }
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class Node {
+//    constructor(data) {
+//        this.data = data;
+//        this.children = [];
+//    }
+//
+//    add(data) {
+//        this.children.push(new Node(data));
+//    }
+//
+//    remove(data) {
+//        this.children = this.children.filter(node => {
+//            return node.data !== data;
+//        });
+//    }
+//}
+//
+//class Tree {
+//    constructor() {
+//        this.root = null;
+//    }
+//
+//    traverseBF(fn) {
+//        const result = [ this.root ];
+//
+//        while (result.length) {
+//            const node = result.shift();
+//            result.push(...node.children);
+//            fn(node);
+//        }
+//
+//    }
+//
+//    traverseDF(fn) {
+//        const result = [ this.root ];
+//
+//        while (result.length) {
+//            const node = result.shift();
+//            result.unshift(...node.children);
+//            fn(node);
+//        }
+//    }
+//}
 
 module.exports = { Tree, Node };
